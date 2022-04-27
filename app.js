5de4f5bca29f6729b7c6288076a44c14b350c5c5
shashards <shane.shardlow@gmail.com>
2022-04-27 12:52:20 +0100

    added rounds

--- app.js
+++ app.js
@@ -10,38 +10,75 @@ function playerPlay() {
     return response.toLowerCase();
 }
 
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
 
-function playRound(playerSelection, computerSelection) {
+
+function playRound() {
+    let result = 0;
+    const computerSelection = computerPlay();
+    const playerSelection = playerPlay();
     if(playerSelection === computerSelection){
         console.log("Draw!")
     } else {
         if(playerSelection === "rock" && computerSelection === "paper"){
             console.log("You Lose! Paper beats Rock");
+            result = 1;
         } else {
             if (playerSelection === "rock" && computerSelection === "scissors"){
                 console.log("You Win! Rock beats Scissors");
+                result = 2;
             } else {
                 if(playerSelection === "paper" && computerSelection === "rock"){
                     console.log("You Win! Paper beats Rocks");
+                    result = 2;
                 } else {
                     if(playerSelection === "paper" && computerSelection === "scissors"){
-                        console.log("You Lose! Scissors beats Paper")
+                        console.log("You Lose! Scissors beats Paper");
+                        result = 1;
                     } else {
                         if(playerSelection === "scissors" && computerSelection === "rock"){
-                            console.log("You Lose! Rock beats Scissors")
+                            console.log("You Lose! Rock beats Scissors");
+                            result = 1;
                         } else {
                             if(playerSelection === "scissors" && computerSelection === "paper"){
-                                console.log("You Win! Scissors beats paper")
+                                console.log("You Win! Scissors beats paper");
+                                result = 2;
                             }
                         }
                     }
                 }
             }
         }
+    } return result
+}
+
+// playRound(playerSelection, computerSelection);
+
+game = () => {
+    playerScore = 0
+    computerScore = 0
+    for (let i = 0; i < 5; i++) {
+        console.log("Player: " + playerScore + " : " + "Computer: " + computerScore)
+        let result = playRound();
+        if(result === 1){
+            computerScore += 1;
+        } else {
+            if(result === 2){
+                playerScore += 1;
+            }
+        } 
+    } console.log("Player: " + playerScore + " : " + "Computer: " + computerScore)
+    if(playerScore === computerScore){
+        console.log("Its a draw!");
+    } else {
+        if(playerScore > computerScore){
+            console.log("Winner Winner")
+        } else {
+            console.log("Loser!")
+        }
     }
 }
 
-playRound(playerSelection, computerSelection);
+game();
+
+
 
